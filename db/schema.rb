845c743a59dacf01af4f74b5ad0209dfdb5648cd
Tiger Rahman <tiger.rahman86@gmail.com>
2015-04-21 20:21:21 -0400

    Cleaned up pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418155818) do
+ActiveRecord::Schema.define(version: 20150421015124) do
+
+  create_table "ideas", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
