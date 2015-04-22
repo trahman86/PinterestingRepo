25862eca5636ae8e37df8da809e26d730dc08f2b
Tiger Rahman <tiger.rahman86@gmail.com>
2015-04-21 21:14:50 -0400

    Updated SQLite

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421015124) do
+ActiveRecord::Schema.define(version: 20150422005838) do
 
   create_table "ideas", force: true do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
